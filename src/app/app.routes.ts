9a40ecbcc84a071a92166fbc5309379127b6ff5b
SergioRiverosDev <sergioriverosdev@gmail.com>
2026-03-18 19:26:09 -0500

    configure el archivo de app.routes.ts con las rutas a trabajar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Categories } from './components/categories/categories';
+import { Products } from './components/products/products';
+import { Product } from './components/product/product';
+import { PayOrder } from './components/pay-order/pay-order';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'categories', component: Categories},
+    {path:'products/:categoryId', component: Products},
+    {path:'product/:id', component:Product},
+    {path:'pay-order', component:PayOrder},
+    {path: '**', redirectTo:'categories'}
+];
