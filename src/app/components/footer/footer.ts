6155de2844a2fcaa49235db54a048cf735d4e4ca
SergioRiverosDev <sergioriverosdev@gmail.com>
2026-03-24 17:29:50 -0500

    agregue la carpeta de imagenes de los productos y realice el header y footer

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
+
+  public year: number = new Date().getFullYear();
 
 }
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatBadge } from '@angular/material/badge';
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, MatIcon,MatBadge],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+
+private traslateService = inject(TranslateService); 
+
+  public languages : string[]= ['en','es'];
+
+
+  changeLenguage(lenguage:string){
+
+    this.traslateService.use(lenguage);
+
+  }
 
 }
